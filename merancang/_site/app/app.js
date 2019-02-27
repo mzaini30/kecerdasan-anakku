becdb10b5f916eff0daa880f03f3492857e81e97
mzaini30 <muhzaini30@gmail.com>
2019-02-27 08:12:36 +0800

    oke

--- merancang/_site/app/app.js
+++ merancang/_site/app/app.js
@@ -1,3 +1,9 @@
 $('.navbar-nav a').click(function(){
 	$('.navbar-toggle').click()
 })
+
+$(document).each('[type=checkbox]', function(){
+	if ($(this).is(':checked')){
+		$(this).addClass('tebal')
+	}
+})
